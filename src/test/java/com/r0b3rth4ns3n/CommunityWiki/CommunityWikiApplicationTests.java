fd4dafa85a0f313f8b864156379fd341d59e2355
Robert Hansen <rob@RobBook.local>
2020-12-04 12:52:24 +0100

    initial commit

---
+++ src/test/java/com/r0b3rth4ns3n/CommunityWiki/CommunityWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r0b3rth4ns3n.CommunityWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
